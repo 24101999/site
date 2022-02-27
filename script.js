50ca5eb165dd196ea4a7c7307f68f8a0d0d40e16
Henrique <81424965+24101999@users.noreply.github.com>
2022-02-27 11:26:55 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,26 +1,47 @@
 const op = document.querySelectorAll('.op')
 const topo = document.querySelector('.bt-top')
-const ball = document.querySelectorAll('.ball')
+const bl = document.querySelectorAll('.ball')
 const aaa = document.getElementById('a')
 const bbb = document.getElementById('b')
 const ccc = document.getElementById('c')
 const bg = document.getElementById('res')
 const bgg = document.querySelectorAll('.res')
+const tog = document.querySelector('.nav-filter')
+const blk = document.querySelector('.blk')
+const preto = document.querySelector('.preto')
+const clos = document.querySelector('.closed')
 
 const input = document.querySelector('.input-form')
 const w = window.screenY / 2
 const sc = innerHeight
 
+setTimeout(() => {
+    preto.classList.add('a')
+}, 2000);
 
+clos.addEventListener('click', () => {
+    preto.classList.remove('a')
+})
+
+
+tog.addEventListener('click', () => {
+    const x = tog.classList.toggle('active')
+    if (x) {
+        blk.style.display = "block"
+
+    } else {
+        blk.style.display = "none"
+    }
+})
 
 op.forEach(function(a, b, c) {
     c.forEach(function(x, y, z) {
-        x.addEventListener('mouseenter', function() {
+        x.addEventListener('mouseenter', () => {
             x.classList.add('active')
             x.classList.remove("desable")
             x.style.color = "#000"
         })
-        x.addEventListener('mouseleave', function() {
+        x.addEventListener('mouseleave', () => {
             x.classList.remove('active')
             x.classList.add('desable')
 
@@ -37,18 +58,18 @@ if (w) {
     topo.style.display = 'block'
 }
 
+const re = bl.forEach((a) => {
+    a.classList.remove('a')
+})
 
-ball.forEach(function(x, c, s) {
-    s.forEach((a, b, c) => {
-        a.addEventListener('click', () => {
-            x.classList.add('a')
-        })
-    })
-    s.forEach((a, b, c) => {
-        a.addEventListener('click', () => {
+bl.forEach(function(x, c, s) {
+    x.addEventListener("click", (element) => {
+        bl.forEach((a) => {
             a.classList.remove('a')
         })
+        x.classList.add('a')
     })
+
 })
 
 aaa.addEventListener('click', function() {
